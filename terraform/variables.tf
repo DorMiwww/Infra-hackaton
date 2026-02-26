1ddd2ba42f99ba078edcfe32bac24ef97504d072
Mykhailo Doronin <mykhailo.doronin@kyivcity.gov.ua>
2026-02-26 15:49:24 +0200

    changes some

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -79,19 +79,19 @@ variable "node_instance_types" {
 variable "node_min_size" {
   description = "Minimum number of nodes in the node group"
   type        = number
-  default     = 3
+  default     = 5
 }
 
 variable "node_max_size" {
   description = "Maximum number of nodes in the node group"
   type        = number
-  default     = 5
+  default     = 8
 }
 
 variable "node_desired_size" {
   description = "Desired number of nodes in the node group"
   type        = number
-  default     = 4
+  default     = 7
 }
 
 # ───────────────────────────────────────────
